9143d12a2ca71460e2ad250dc18e14d867d7043a
Epat <i.am@epat.xyz>
2021-07-13 08:50:32 +0200

    Interrupts

--- GBEmulator/Emu/GbCpu.cpp
+++ GBEmulator/Emu/GbCpu.cpp
@@ -1,29 +1,87 @@
+#include <iostream>
 #include "GbCpu.h"
 
 
 namespace Emulator {
 	GbCpu::GbCpu()
 	{
+
 	}
 
-	GbCpu::~GbCpu()
-	{
+	GbCpu::~GbCpu() = default;
+
+	void GbCpu::Connect(Emulator::GbBus* bus) {
+        this->Bus = bus;
 	}
 
-	void GbCpu::Connect(Emulator::Bus* bus) {
-        this->bus = bus;
-	}
+    bool executingInstruction = false; // temporary
+    void GbCpu::OnClockCycle() {
+        if(InterruptMasterEnable && !executingInstruction && ((GetInterruptFlags() & Bus->InterruptEnableRegister) > 0)) {
+            PrepareForInterrupt();
+        }
+
+        if(!ExecutionSteps.empty()){
+            ExecutionSteps.front()();
+            ExecutionSteps.pop();
+        }
+
+    }
+
+    void GbCpu::PrepareForInterrupt() {
+        InterruptMasterEnable = false;
+
+        // Interrupt Service Routine
+        //According to Z80 datasheets, the following occurs when control is being transferred to an interrupt
+        //handler:
+        //1. Two wait states are executed (2 machine cycles pass while nothing occurs, presumably the CPU
+        //is executing NOPs during this time).
+        //2. The current PC is pushed onto the stack, this process consumes 2 more machine cycles.
+        //3. The high byte of the PC is set to 0, the low byte is set to the address of the handler
+        //($40,$48,$50,$58,$60). This consumes one last machine cycle.
+
+        uint16_t pc = 0x00;
+
+        //one step per clock cycle
+        ExecutionSteps.push([]{});
+        ExecutionSteps.push([]{});
+        ExecutionSteps.push([&pc, this]{ pc = Registers.PC; });
+        ExecutionSteps.push([pc, this] { StackPush(pc); });
+        ExecutionSteps.push([this] {
+            auto intrps = GetInterruptFlags() & Bus->InterruptEnableRegister;
+
+            if((intrps & (uint8_t)Interrupts::VerticalBlankInterrupt)) {
+                Registers.PC = 0x0040;
+            }
+            else if((intrps & (uint8_t)Interrupts::LcdStatInterrupt)) {
+                Registers.PC = 0x0048;
+            }
+            else if((intrps & (uint8_t)Interrupts::TimerInterrupt)) {
+                Registers.PC = 0x0050;
+            }
+            else if((intrps & (uint8_t)Interrupts::SerialInterrupt)) {
+                Registers.PC = 0x0058;
+            }
+            else if((intrps & (uint8_t)Interrupts::JoypadInterrupt)) {
+                Registers.PC = 0x0060;
+            }
+            
+            executingInstruction = false;
+        });
+
+        executingInstruction = true;
+    }
+
 
 	void GbCpu::Write(uint16_t address, uint8_t data)
 	{
-	    if(bus != nullptr)
-		    bus->Write(address, data);
+	    if(Bus != nullptr)
+		    Bus->Write(address, data);
 	}
 
 	uint8_t GbCpu::Read(uint16_t address)
 	{
-	    if(bus != nullptr)
-		    return bus->Read(address, false);
+	    if(Bus != nullptr)
+		    return Bus->Read(address, false);
 	    return 0x00;
 	}
 
@@ -35,8 +93,21 @@ namespace Emulator {
         Registers.FlagsState &= ~flag;
     }
 
-    bool GbCpu::IsFlagSet(GbCpu::Flags flag){
+    bool GbCpu::IsFlagSet(GbCpu::Flags flag) {
         return (Registers.FlagsState & flag);
     }
+
+    uint8_t GbCpu::GetInterruptFlags() {
+        return Read(0xFF0F) & 0b00011111; // only lower 5 bits are interrupt flags
+    }
+
+    uint8_t GbCpu::StackPop() {
+        return Read(Registers.SP++);
+    }
+
+    void GbCpu::StackPush(uint8_t value) {
+        Write(Registers.SP--, value);
+    }
+
 
 }
--- GBEmulator/Emu/GbCpu.h
+++ GBEmulator/Emu/GbCpu.h
@@ -2,13 +2,15 @@
 #ifndef CPU_H
 #define CPU_H
 
+#include <queue>
+#include <functional>
 
-#include "Bus.h"
+#include "GbBus.h"
 #include "CpuRegisters.h"
 
 namespace Emulator
 {
-	class Bus;
+	class GbBus;
 
 	class GbCpu
 	{
@@ -16,22 +18,42 @@ namespace Emulator
 		GbCpu();
 		~GbCpu();
 
-		void Connect(Bus* bus);
+		void Connect(GbBus* bus);
+
+        enum class Interrupts {
+            VerticalBlankInterrupt = 1,
+            LcdStatInterrupt       = 1 << 1,
+            TimerInterrupt         = 1 << 2,
+            SerialInterrupt        = 1 << 3,
+            JoypadInterrupt        = 1 << 4,
+        };
 
 	private:
-		Bus* bus = nullptr;
+		GbBus* Bus = nullptr;
 
 		void Write(uint16_t address, uint8_t data);
 		uint8_t Read(uint16_t address);
 
+        uint8_t GetInterruptFlags();
 
+        bool InterruptMasterEnable = true; // false if executing an interrupt handler
+        bool Halt = false;
+        bool Stop = false;
+
+        void StackPush(uint8_t value);
+        uint8_t StackPop();
+
+        void PrepareForInterrupt();
+
+        // contains steps required for an instruction to work
+        std::queue<std::function<void()>> ExecutionSteps;
 
 	public:
 		enum Flags {
-			Zero = (1 << 7),
+			Zero        = (1 << 7),
 			Subtraction = (1 << 6), // BCD
-			HalfCarry = (1 << 5), // BCD
-			Carry = (1 << 4),
+			HalfCarry   = (1 << 5), // BCD
+			Carry       = (1 << 4),
 		};
 
         CpuRegisters Registers;
@@ -40,6 +62,9 @@ namespace Emulator
 		void UnsetFlag(Flags flag);
 
 		bool IsFlagSet(Flags flag);
+
+
+		void OnClockCycle();
 
 	};
 }
