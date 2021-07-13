91dfd64957230744518708696549e67392d6ff3f
Epat <i.am@epat.xyz>
2021-07-13 08:01:34 +0200

    just to get all windows definitions in one place

---
+++ GBEmulator/Windows/Windows.h
@@ -0,0 +1,15 @@
+//
+// Created by epat on 11.07.2021.
+//
+
+#ifndef GBEMU_WINDOWS_H
+#define GBEMU_WINDOWS_H
+
+namespace Windows{
+    class Main;
+    class Registers;
+}
+#include "MainWindow.h"
+#include "Registers.h"
+
+#endif //GBEMU_WINDOWS_H
